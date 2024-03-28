679b18156892ca0304cd754b74f31140e9fbb71d
Muhamad Rafli Susanto <73054214+raflisusanto@users.noreply.github.com>
2024-03-28 14:10:38 +0700

    feat: added particles loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,53 @@
 import React from "react";
 import Sidebar from "./components/sidebar/Sidebar";
 import Navbar from "./components/navbar/Navbar";
-import ParticlesBackground from "./components/particles/ParticlesBackground";
 import ProjectsSection from "./components/section/ProjectsSection";
 import AboutSection from "./components/section/AboutSection";
 import HomeSection from "./components/section/HomeSection";
 import FooterSection from "./components/section/FooterSection";
+import { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles";
+import particlesConfig from "./components/config/particles-config";
+import Loading from "./components/ui/Loading";
 
 const App = () => {
-  return (
-    <>
-      {/* Particles */}
-      <ParticlesBackground />
+  const [init, setInit] = useState(false);
 
-      {/* Navigations */}
-      <Sidebar />
-      <Navbar />
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
 
-      {/* Main Sections */}
-      <HomeSection />
-      <AboutSection />
-      <ProjectsSection />
-      <FooterSection />
-    </>
-  );
+  if (init) {
+    // Particles has loaded
+    return (
+      <>
+        {/* Particles */}
+        <Particles
+          id="tsparticles"
+          options={particlesConfig}
+          className="absolute -z-[2]"
+        />
+
+        {/* Navigations */}
+        <Sidebar />
+        <Navbar />
+
+        {/* Main Sections */}
+        <HomeSection />
+        <AboutSection />
+        <ProjectsSection />
+        <FooterSection />
+      </>
+    );
+  }
+
+  // Add some loading before particles loaded
+  return <Loading />;
 };
 
 export default App;
